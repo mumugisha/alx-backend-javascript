451aaea60fdfc94ca2e45ecde8e5f266b46a092c
Alphonse Mugisha <79317323+mumugisha@users.noreply.github.com>
2024-10-02 22:12:30 +0200

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 /* Return a Promise using this prototype */
 export function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve('Success');
+  return new Promise((resolve) => {
+    resolve(true);
   });
 }
