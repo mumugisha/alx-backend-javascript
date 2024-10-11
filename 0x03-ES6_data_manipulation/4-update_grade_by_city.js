9d24113fa4340b0b11af11b1ca23112dc43c8efb
mumugisha <prudent.nkubito@gmail.com>
2024-10-11 21:33:36 +0300

    Python - data manipulation

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+// 4-update_grade_by_city.js
+
+export function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
