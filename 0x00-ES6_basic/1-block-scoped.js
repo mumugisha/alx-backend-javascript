5013f5b081cf4a6d2af369097187a9a616d5c9fb
mumugisha <prudent.nkubito@gmail.com>
2024-10-20 13:03:10 +0300

    ES6 Basics

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,11 @@
-export function taskBlock(trueOrFalse) {
-  const task = trueOrFalse;
-  const task2 = !trueOrFalse;
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
 
   return [task, task2];
 }
