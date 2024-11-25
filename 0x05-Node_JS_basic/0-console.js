482ba1b3d850d10876e5cfcd2008cde92e728b81
mumugisha <prudent.nkubito@gmail.com>
2024-11-25 11:10:52 +0200

    NodeJS Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// Create a function named displayMessage that prints in STDOUT the string argument.
+function displayMessage(data) {
+  console.log(data);
+}
+
+// Export the displayMessage function explicitly
+module.exports = displayMessage;
