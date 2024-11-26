db34cd2a0837b6c762b172782246b1de04399ae5
mumugisha <prudent.nkubito@gmail.com>
2024-11-26 14:55:19 +0200

    NodeJS Basics

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import fs from 'fs';
 
 function countStudents(NameOfFile) {
   const students = {};
@@ -8,39 +8,40 @@ function countStudents(NameOfFile) {
   try {
     const dataContents = fs.readFileSync(NameOfFile, 'utf8');
     const lines = dataContents.toString().split('\n');
+    
     for (let a = 1; a < lines.length; a += 1) {
-        if (lines[a]) {
-           length += 1;
-      const field = lines[a].toString().split(',');
-      if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-          students(field[3].push(field[0]);
-      } else {
+      if (lines[a]) {
+        length += 1;
+        const field = lines[a].toString().split(',');
+        
+        if (!students[field[3]]) {
+          students[field[3]] = [];
+        }
         students[field[3]].push(field[0]);
-    }
-    if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-         field[field[3] += 1;
-    } else {
-      fields(field[3]] = 1;
-    }
-  }
-}
-    const 1 = length - 1;
-    console.log(`Number of students: ${1}`);
-    for (const [key, value] of Object.entries(fields)) {
-      if (key !== 'field') {
-      console.log(
-        `Number of students in ${key}: ${value}. List: ${student
+        
+        if (fields[field[3]]) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
       }
     }
+
+    console.log(`Number of students: ${length}`);
+    
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(
+          `Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`
+        );
+      }
+    }
+
   } catch (error) {
     console.error('Cannot load the database');
     throw error;
-    }
   }
- }
- catch (error) {
-     throw Error("Cannot load the database");
- }
 }
 
-module.exports = countStudents;
+export default countStudents;
+
