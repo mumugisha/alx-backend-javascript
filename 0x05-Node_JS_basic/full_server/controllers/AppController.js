f031b6ae3249766fd60591edab6a98e3eb0145a9
mumugisha <prudent.nkubito@gmail.com>
2024-11-27 06:34:50 +0000

    NodeJS Basics

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -2,7 +2,7 @@ class AppController {
   static getHomepage (request, response) {
     response
       .status(200)
-      .send('Hello Holberton School');
+      .send('Hello Holberton School!');
   }
 }
 
