70ed7db27da57c0d604a6b4821ac8cae9ec9b389
mumugisha <prudent.nkubito@gmail.com>
2024-11-28 00:01:21 +0000

    NodeJs Basics

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,18 @@
+const { describe, it } = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+const assert = require("assert");
+
+describe("sendPaymentRequestToApi", function () {
+  it("should call Utils.calculateNumber once", function () {
+    const spy = sinon.spy(Utils, "calculateNumber");
+    try {
+      sendPaymentRequestToApi(50, 24.52);
+      assert(spy.calledOnce);
+      assert(spy.calledWith("SUM", 50, 24.52)); // Ensure correct arguments
+    } finally {
+      spy.restore();
+    }
+  });
+});
