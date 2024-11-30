0a174c09325be49538c56307bcb02c93b35eb350
mumugisha <prudent.nkubito@gmail.com>
2024-11-30 20:44:56 +0000

    NodeJS Basics

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,19 +1,13 @@
-/**
- * Basic Express server that listens on port 1245.
- * Provides a single route at '/' that responds with a greeting.
- */
-
 const express = require('express');
 
 const app = express();
 const port = 1245;
 
-// Route to respond with a greeting message
 app.get('/', (request, response) => {
   response.send('Hello Holberton School!');
 });
 
-// Start the server
-app.listen(port, () => {});
+app.listen(port, () => {
+});
 
-module.exports = { app };
+module.exports = app;
